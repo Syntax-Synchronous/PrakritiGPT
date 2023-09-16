00875468de1ca7689cc5b1893c2a5eab90d4d7d5
SnShot04 <sub.assasin@gmail.com>
2023-09-17 03:26:07 +0530

    working bot just need to store prakriti data

--- get_intent.py
+++ get_intent.py
@@ -8,6 +8,9 @@ import numpy as np
 import pandas as pd
 nl_model = load('./nlm')
 
+with open('intents.json', 'r') as f:
+    intents = json.load(f)
+
 intentList = {
     2: 'greeting',
     1: 'goodbye',
@@ -22,7 +25,7 @@ df = pd.read_csv("./sentences_intent.csv",names = ['Sentences', 'target'])
 X_train=df.Sentences
 
 def get_intent(msg_count):
-    ans = np.argmax(nl_model.predict(msg_count))
+    ans = np.argmax(nl_model.predict(msg_count,verbose = 0))
     return intentList.get(ans)
 
 vectorizer = CountVectorizer(min_df=1)
@@ -32,6 +35,10 @@ def get_response(msg):
     msg_list= []
     msg_list.append(msg)
     msg_count = vectorizer.transform(msg_list)
-    print(get_intent(msg_count))
-    
-get_response("thank you")
+    tag = get_intent(msg_count)
+    for intent in intents["intent"]:
+        if tag == intent["tag"]:
+            ans = random.choice(intent["responses"])
+            return (ans)
+
+
